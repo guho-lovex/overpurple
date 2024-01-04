f311bd6c2a5c646a6cc8dabd2a22f2b8f3ce586a
guho-lovex <l2276227464@163.com>
2024-01-04 15:51:18 +0800

    recove rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,5 +77,55 @@ module.exports = {
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet-async`,
     `gatsby-plugin-sitemap`,
+
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+        {
+          site {
+            siteMetadata {
+              title
+              description
+              siteUrl
+              site_url: siteUrl
+            }
+          }
+        }
+      `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.nodes.map(node => {
+                return Object.assign({}, node.frontmatter, {
+                  description: node.excerpt,
+                  date: node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + node.fields.slug,
+                  custom_elements: [{ 'content:encoded': node.html }],
+                });
+              });
+            },
+            query: `{
+            allMarkdownRemark(sort: {frontmatter: {date: DESC}}) {
+              nodes {
+                excerpt
+                html
+                fields {
+                  slug
+                }
+                frontmatter {
+                  title
+                  date
+                }
+              }
+            }
+          }`,
+            output: '/rss.xml',
+            title: 'A Blog RSS Feed',
+          },
+        ],
+      },
+    },
   ],
 };
